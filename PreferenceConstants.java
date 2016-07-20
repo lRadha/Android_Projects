f3006c1dd9fba598e0be278cb131ee52b4933187
lRadha <lkrdkrishnan@gmail.com>
2016-07-20 11:15:21 +0530

    Create PreferenceConstants.java

---
+++ PreferenceConstants.java
@@ -0,0 +1,30 @@
+public class PreferencesConstants {
+	// Application Preferences
+	public static final String USER_EMAIL_AS_ID = "user_email_as_id";
+	public static final String  REGISTRATION_DONE = "registration_done";
+	public static final String  DEVICE_REGISTRATION_DONE = "device_registration_done";
+	public static final String  LOGGED_IN = "logged_in";
+	public static final String SECURITY_CODE = "security_code";
+	public static final String WE_WERE_ON_LOCATION = "we_were_on_location_page";
+	public static final String FCM_TOKEN = "fcm_token";
+	public static final String SENT_TOKEN_TO_SERVER = "sent_token_to_server";
+
+	// Settings Preferences
+	public static final String WIPE_DATA = "wipe_data";
+	public static final String SENT_ALERT_VIA_EMAIL = "sent_alert_via_email";
+	public static final String GET_LOCATION = "get_location";
+	public static final String START_TRACKING = "start_tracking";
+	public static final String GET_DEVICE_INFO = "get_device_info";
+	public static final String GET_LOCATION_HISTORY = "get_location_history";
+	public static final String GET_CALL_LOG = "get_call_log";
+	public static final String LOCK_WITH_CODE = "lock_with_code";
+	public static final String DISPLAY_MESSAGE = "display_message";
+
+	// State Preferences
+	public static final String ALARM_ON = "alarm_on";
+	public static final String DEVICE_LOCKED = "device_locked";
+	public static final String DEVICE_LOST = "device_lost";
+	public static final String TRACK_ON = "track_on";
+	public static final String PICTURES_CAPTURED = "pictures_captured";
+	public static final String VIDEOS_CAPTURED = "videos_captured";
+}
